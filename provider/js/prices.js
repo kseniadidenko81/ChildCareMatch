7a8767d60ea7a877a91091bd3e9e9ef29eee1b1a
kseniadidenko81 <57663840+kseniadidenko81@users.noreply.github.com>
2025-04-02 10:56:19 +0300

    Update prices.js11

--- provider/js/prices.js
+++ provider/js/prices.js
@@ -161,8 +161,16 @@ document.addEventListener("DOMContentLoaded", function () {
     const tooltipTriggerList = [].slice.call(
       document.querySelectorAll('[data-bs-toggle="tooltip"]')
     );
+
     tooltipTriggerList.map(function (tooltipTriggerEl) {
-      return new bootstrap.Tooltip(tooltipTriggerEl);
+      const tooltip = new bootstrap.Tooltip(tooltipTriggerEl);
+
+      tooltipTriggerEl.addEventListener("shown.bs.tooltip", function () {
+        const tooltipElement = document.querySelector(".tooltip-inner");
+        if (tooltipElement) {
+          tooltipElement.classList.add("custom-tooltip");
+        }
+      });
     });
   }
 
